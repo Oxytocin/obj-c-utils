5706994050bce9d2b88d2c69473de64e2d072bbc
Anton Yakovenko <anton.yakovenko1991@gmail.com>
2015-04-22 19:45:01 +0300

    Added Sample Manager Class

---
+++ SampleManager.h
@@ -0,0 +1,15 @@
+//
+//  SampleManager.h
+//  mmo
+//
+//  Created by Anton Yakovenko on 22.04.15.
+//  Copyright (c) 2015 Win-Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleManager : NSObject
+
+- (void)test;
+
+@end
---
+++ SampleManager.m
@@ -0,0 +1,17 @@
+//
+//  SampleManager.m
+//  mmo
+//
+//  Created by Anton Yakovenko on 22.04.15.
+//  Copyright (c) 2015 Win-Interactive. All rights reserved.
+//
+
+#import "SampleManager.h"
+
+@implementation SampleManager
+
+- (void)test {
+    NSLog(@"Log from Sample Manager");
+}
+
+@end
